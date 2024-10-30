418e00d5db8f43f2d5f8d669e1515fa723d00928
mirkoEscobedo <escobedomirko@gmail.com>
2024-10-30 21:25:20 +0100

    added example cardgrid to be removed

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,14 @@
-import ConvenienceIcon from "@/components/convenienceIcon/ConvenienceIcon";
-import SearchBox from "@/components/searchBox/SearchBox";
-import ShieldIcon from "@/components/shieldIcon/ShieldIcon";
-import SimplicityIcon from "@/components/simplicityIcon/SimplicityIcon";
-import Title from "@/components/title/Title";
-import TopBar from "@/components/topBar/TopBar";
-import WhyChooseUsParent from "@/components/whyChooseUsParent/WhyChooseUsParent";
-import { useLanguage } from "@/context/LanguageContex";
-import DownloadIcon from "@/shared/components/downloadIcon/DownloadIcon";
-import TextCard from "@/shared/components/textCard/TextCard";
+import CardGrid, { sampleData } from '@/components/card_grid/CardGrid';
+import ConvenienceIcon from '@/components/convenienceIcon/ConvenienceIcon';
+import SearchBox from '@/components/searchBox/SearchBox';
+import ShieldIcon from '@/components/shieldIcon/ShieldIcon';
+import SimplicityIcon from '@/components/simplicityIcon/SimplicityIcon';
+import Title from '@/components/title/Title';
+import TopBar from '@/components/topBar/TopBar';
+import WhyChooseUsParent from '@/components/whyChooseUsParent/WhyChooseUsParent';
+import { useLanguage } from '@/context/LanguageContex';
+import DownloadIcon from '@/shared/components/downloadIcon/DownloadIcon';
+import TextCard from '@/shared/components/textCard/TextCard';
 
 const Home: React.FC = () => {
   const { translations } = useLanguage();
@@ -16,6 +17,7 @@ const Home: React.FC = () => {
       <TopBar></TopBar>
       <Title />
       <SearchBox />
+      <CardGrid data={sampleData}></CardGrid>
       <WhyChooseUsParent>
         <TextCard
           title={translations.convenienceTitle}
