8ccc5aafd60a2035fc86414bcc2a43a3480ca280
mirkoEscobedo <escobedomirko@gmail.com>
2024-10-31 15:19:42 +0100

    added extra classes as optional props

--- src/shared/components/element_card/ElementCard.tsx
+++ src/shared/components/element_card/ElementCard.tsx
@@ -6,12 +6,14 @@ import defaultImage from '@/assets/default_image.png';
 import Chan from '@/components/chan/Chan';
 
 interface ElementCardProps {
+  extraClasses?: string;
   icon?: string;
   thumbnail?: string;
   title: string;
   onClick?: () => void;
 }
 const ElementCard: React.FC<ElementCardProps> = ({
+  extraClasses,
   icon,
   thumbnail,
   title,
@@ -34,7 +36,9 @@ const ElementCard: React.FC<ElementCardProps> = ({
   };
   return (
     <>
-      <div className="flex flex-col items-center m-4 rounded-md hover:shadow-lg transition-shadow max-w-xs">
+      <div
+        className={`flex flex-col items-center m-4 rounded-md hover:shadow-lg transition-shadow max-w-xs ${extraClasses}`}
+      >
         <div className="w-full overflow-hidden rounded-mb mb-1 aspect-w-16 aspect-h-9">
           <img
             src={thumbnail ? thumbnail : defaultImage}
