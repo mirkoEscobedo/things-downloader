cde019541031969fbc27f297bba64191977622c9
mirkoEscobedo <escobedomirko@gmail.com>
2024-11-09 16:41:43 +0100

    added utils function that construct the cards array based on inputdata

---
+++ src/utils/constructElementCards.ts
@@ -0,0 +1,34 @@
+import { ElementCardType } from '@/typedef/typedef';
+
+interface MediaItem {
+  filename: string;
+  url: string;
+}
+
+interface MediaResponse {
+  result: {
+    data: MediaItem[];
+  };
+}
+
+export function constructElementCards(
+  response: MediaResponse
+): ElementCardType[] {
+  const { data } = response.result;
+
+  const cards: ElementCardType[] = data.map((item) => {
+    let icon = 'default';
+
+    if (item.url.includes('4cdn')) {
+      icon = '4chan';
+    } else if (
+      item.url.includes('youtube.com') ||
+      item.url.includes('youtu.be')
+    ) {
+      icon = 'youtube';
+    }
+    return { title: item.filename, thumbnail: item.url, icon };
+  });
+
+  return cards;
+}
