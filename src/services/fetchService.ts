a4330125d40f2ca238963ae3283692ac9706c8fe
mirkoEscobedo <escobedomirko@gmail.com>
2024-11-09 16:09:30 +0100

    final version of the fetcher

--- src/services/fetchService.ts
+++ src/services/fetchService.ts
@@ -1,12 +1,23 @@
+import { CHAN } from '@/const/const';
+
 export async function fetchData(url: string) {
   try {
-    const response = await fetch(
-      `http://localhost:4000/trpc/media.getChanMediaList?input={"link":"${url}"}`
-    );
+    let endpoint: string = '';
+
+    switch (url) {
+      case CHAN:
+        endpoint = `http://localhost:4000/trpc/media.getChanMediaList?input={"link":"${url}"}`;
+        break;
+      default:
+        throw new Error('Unsupported platform link.');
+    }
+    const response = await fetch(endpoint);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status ${response.status}`);
+    }
     const data = await response.json();
     return data;
   } catch (error) {
-    console.log(error);
-    
+    throw error;
   }
 }
