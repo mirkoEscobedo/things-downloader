d27c2865f40f742c01ad37c04dd4f7c106ae11a1
circumspectjunior <giftojo200047@yahoo.com>
2024-11-14 20:29:21 +0100

    translationUpdated

--- src/components/whyChooseUsParent/WhyChooseUsParent.tsx
+++ src/components/whyChooseUsParent/WhyChooseUsParent.tsx
@@ -1,13 +1,15 @@
+import { useLanguage } from "@/context/LanguageContex";
 import "./WhyChooseUsParent.css";
 type WhyChooseUsParentProps = {
   children: React.ReactNode;
 }; //sto dichiarando il tipo
 
 const WhyChooseUsParent: React.FC<WhyChooseUsParentProps> = ({ children }) => {
+  const { translations } = useLanguage();
   return (
     <>
       <h2 id="whyHeading" className="bg-transparent">
-        Why Choose Us?
+        {translations.whyUsText}
       </h2>
       <section className="md:grid-cols-2 lg:grid-cols-3 bg-transparent">
         {children}
