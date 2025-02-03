c5963730d8b6d0424ee68fa4705803d5c980586b
mirkoEscobedo <44160813+mirkoEscobedo@users.noreply.github.com>
2025-02-03 12:17:54 +0100

    ...

--- src/downloadWorker.ts
+++ src/downloadWorker.ts
@@ -1,7 +1,7 @@
-import { ElementCardType } from './typedef/typedef';
+import { ElementCardType } from "./typedef/typedef";
 
 self.onmessage = async (e: MessageEvent) => {
-  const cards: ElementCardType[] = e.data;
+  const cards: ElementCardType[] = e.data.toDownload;
   let downloadedFiles: { video: Blob; title: string | undefined }[] = [];
   try {
     for (const card of cards) {
@@ -11,21 +11,23 @@ self.onmessage = async (e: MessageEvent) => {
 
     self.postMessage(downloadedFiles);
   } catch (err) {
-    console.error('failed: ', err);
+    console.error("failed: ", err);
   }
 };
 
 async function downloadMediaFiles(card: ElementCardType) {
+  console.log(card);
   try {
     if (
-      !card.url.startsWith('https://i.4cdn.org/') &&
-      !card.url.startsWith('https://a.4cdn.org/')
+      !card.url.startsWith("https://i.4cdn.org/") &&
+      !card.url.startsWith("https://a.4cdn.org/")
     ) {
       throw new Error(`Invalid link ${card.url}`);
     }
     new URL(card.url);
     const url = `http://localhost:4000/proxy/${encodeURIComponent(card.url)}`;
     const response = await fetch(url);
+    console.log(response);
     if (!response.ok) {
       throw new Error(`HTTP error, status: ${response.status}`);
     }
