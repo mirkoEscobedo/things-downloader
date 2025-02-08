6922335ac648145f2a35a3f36554b5f266fc0667
mirkoEscobedo <escobedomirko@gmail.com>
2025-02-08 20:01:52 +0100

    inserted rate limiters

--- src/components/resultCard/ResultCard.tsx
+++ src/components/resultCard/ResultCard.tsx
@@ -1,20 +1,20 @@
-import GeneralCard from "@/shared/components/general_card/General_Card";
-import GeneralButton from "@/shared/components/generalButton/GeneralButton";
-import { ElementCardType } from "@/typedef/typedef";
+import GeneralCard from '@/shared/components/general_card/General_Card';
+import GeneralButton from '@/shared/components/generalButton/GeneralButton';
+import { ElementCardType } from '@/typedef/typedef';
 import {
   addDownloadToHistory,
   getDownloadHistory,
-} from "@/utils/downloadHistory";
-import { ProgessView } from "../progress_view/ProgressView";
-import { DownloadIcon, X } from "lucide-react";
-import ConvertSelector from "../convertSelector/ConvertSelector";
-import DownloadCardList from "../downloadCardList/DownloadCardList";
-import "./resultCard.css";
-import { useAppDispatch, useAppSelector } from "@/hooks/hooks";
-import { setDownloadHistory } from "@/state/reducers/downloadHistorySlice";
-import { resetList } from "@/state/reducers/selectedToDownloadSlice";
-import { startDownload } from "@/utils/startWorkers";
-import FFmpegSingleton from "@/utils/ffmpegSingleton";
+} from '@/utils/downloadHistory';
+import { ProgessView } from '../progress_view/ProgressView';
+import { DownloadIcon, X } from 'lucide-react';
+import ConvertSelector from '../convertSelector/ConvertSelector';
+import DownloadCardList from '../downloadCardList/DownloadCardList';
+import './resultCard.css';
+import { useAppDispatch, useAppSelector } from '@/hooks/hooks';
+import { setDownloadHistory } from '@/state/reducers/downloadHistorySlice';
+import { resetList } from '@/state/reducers/selectedToDownloadSlice';
+import { startDownload } from '@/utils/startWorkers';
+import FFmpegSingleton from '@/utils/ffmpegSingleton';
 
 interface ResultCardProps {
   downloadCardList: ElementCardType[];
@@ -33,17 +33,17 @@ const ResultCard: React.FC<ResultCardProps> = ({
     (state) => state.selectToDownload.list
   );
   const format = useAppSelector((state) => state.selectFormat.format);
-  const ffmpeg = FFmpegSingleton.getFFmpeg();
 
   const handleDownloadAll = async () => {
     try {
+      const ffmpeg = await FFmpegSingleton.getFFmpeg();
       const result = await startDownload(
         selectedToDownload.length > 0 ? selectedToDownload : downloadCardList,
         ffmpeg,
         format
       );
       const url = URL.createObjectURL(result.blob);
-      const a = document.createElement("a");
+      const a = document.createElement('a');
       a.href = url;
       a.download = result.fileName;
       document.body.appendChild(a);
@@ -65,9 +65,9 @@ const ResultCard: React.FC<ResultCardProps> = ({
             dispatch(setDownloadHistory(getDownloadHistory()));
           });
 
-      console.log("Download successfull");
+      console.log('Download successfull');
     } catch (err) {
-      console.error("Download Failed: ", err);
+      console.error('Download Failed: ', err);
     }
   };
 
@@ -77,7 +77,7 @@ const ResultCard: React.FC<ResultCardProps> = ({
 
   return (
     <>
-      <GeneralCard className={`mt-6 justify-self-center ${className || ""}`}>
+      <GeneralCard className={`mt-6 justify-self-center ${className || ''}`}>
         {downloadCardList.length > 1 && (
           <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 items-center w-full mt-4 mb-2">
             <div className="flex items-center justify-center text-xl">
