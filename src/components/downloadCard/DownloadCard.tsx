ba187e2689b5ff6cd46a3bccb6f83c09dbc775fe
mirkoEscobedo <escobedomirko@gmail.com>
2025-02-09 19:07:15 +0100

    fixed forgotten props on single download

--- src/components/downloadCard/DownloadCard.tsx
+++ src/components/downloadCard/DownloadCard.tsx
@@ -1,20 +1,20 @@
-import ElementCard from "@/shared/components/element_card/ElementCard";
-import GeneralButton from "@/shared/components/generalButton/GeneralButton";
+import ElementCard from '@/shared/components/element_card/ElementCard';
+import GeneralButton from '@/shared/components/generalButton/GeneralButton';
 import {
   addDownloadToHistory,
   getDownloadHistory,
-} from "@/utils/downloadHistory";
-import { DownloadIcon } from "lucide-react";
-import { useAppDispatch, useAppSelector } from "@/hooks/hooks";
-import { setDownloadHistory } from "@/state/reducers/downloadHistorySlice";
-import { setTrue } from "@/state/reducers/downloadSlice";
-import { ElementCardType } from "@/typedef/typedef";
-import { startDownload } from "@/utils/startWorkers";
+} from '@/utils/downloadHistory';
+import { DownloadIcon } from 'lucide-react';
+import { useAppDispatch, useAppSelector } from '@/hooks/hooks';
+import { setDownloadHistory } from '@/state/reducers/downloadHistorySlice';
+import { setTrue } from '@/state/reducers/downloadSlice';
+import { ElementCardType } from '@/typedef/typedef';
+import { startDownload } from '@/utils/startWorkers';
 import {
   addSelectedToDownload,
   removeSelectedToDownload,
-} from "@/state/reducers/selectedToDownloadSlice";
-import FFmpegSingleton from "@/utils/ffmpegSingleton";
+} from '@/state/reducers/selectedToDownloadSlice';
+import FFmpegSingleton from '@/utils/ffmpegSingleton';
 
 interface DonwloadCardProps {
   card: ElementCardType;
@@ -27,7 +27,6 @@ const DonwloadCard: React.FC<DonwloadCardProps> = ({ card, onClick }) => {
   const format = useAppSelector((state) => state.selectFormat.format);
   const selectedCards = useAppSelector((state) => state.selectToDownload.list);
   const isChecked = selectedCards.some((item) => item.url === card.url);
-  const ffmpeg = FFmpegSingleton.getFFmpeg();
 
   const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newChecked = event.target.checked;
@@ -42,26 +41,28 @@ const DonwloadCard: React.FC<DonwloadCardProps> = ({ card, onClick }) => {
     const cards = [card];
     dispatch(setTrue());
     try {
+      const ffmpeg = await FFmpegSingleton.getFFmpeg();
       const result = await startDownload(cards, ffmpeg, format);
       const url = URL.createObjectURL(result.blob);
-      const a = document.createElement("a");
+      const a = document.createElement('a');
       a.href = url;
       a.download = result.fileName;
       document.body.appendChild(a);
       a.click();
       document.body.removeChild(a);
       URL.revokeObjectURL(url);
-      console.log("Download successfull: ", url);
+      console.log('Download successfull: ', url);
       addDownloadToHistory({
         title: card.title,
         icon: card.icon,
         thumbnail: card.thumbnail,
         url: card.url,
+        ext: card.ext,
       });
 
       dispatch(setDownloadHistory(getDownloadHistory()));
     } catch (err) {
-      console.error("Download Failed: ", err);
+      console.error('Download Failed: ', err);
     }
   };
 
@@ -73,9 +74,10 @@ const DonwloadCard: React.FC<DonwloadCardProps> = ({ card, onClick }) => {
             url={card.url}
             title={card.title}
             icon={card.icon}
-            thumbnail={card.thumbnail == null ? "" : card.thumbnail}
+            thumbnail={card.thumbnail == null ? '' : card.thumbnail}
             onClick={onClick}
             extraClasses=""
+            ext={card.ext}
           ></ElementCard>
         </div>
         <div className="flex items-center justify-center">
